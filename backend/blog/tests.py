5dfcf8394f65238ca95bed287f46fd785b697c66
Yuko Iinuma <iinuma0710@gmail.com>
2025-04-07 23:36:25 +0900

    20250407 テンプレートエンジンを使って各ページを実装

--- backend/blog/tests.py
+++ backend/blog/tests.py
@@ -1,34 +1,85 @@
-from django.test import TestCase
+from django.test import TestCase, Client, RequestFactory
 from django.urls import resolve
+from django.contrib.auth import get_user_model
 
+from blog.models import Article
 from blog.views import top, article_new, article_edit, article_detail
 
+UserModel = get_user_model()
 
-class TopPageViewTest(TestCase):
-    # トップページにアクセスしたときにステータスコード 200 が返ってくるか
-    def test_top_returns_200(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
 
-    # トップページから返ってきたリクエストボディが b"Hello World!" か
-    def test_top_returns_expected_content(self):
-        response = self.client.get("/")
-        self.assertEqual(response.content, b"Hello World!")
+class TopPageTest(TestCase):
+    def setUp(self):
+        self.user = UserModel.objects.create(
+            username="test_user",
+            email="test@example.com",
+            password="top_secret_pass0001",
+        )
+        self.article = Article.objects.create(
+            title="title1",
+            abstract="abstract",
+            body="body",
+            created_by=self.user
+        )
+
+    def test_should_return_article_title(self):
+        request = RequestFactory().get("/")
+        request.user = self.user
+        response = top(request)
+        self.assertContains(response, self.article.title)
+
+    def test_should_return_username(self):
+        request = RequestFactory().get("/")
+        request.user = self.user
+        response = top(request)
+        self.assertContains(response, self.user.username)
 
 
 class CreateArticleTest(TestCase):
-    def test_should_resolve_article_new(self):
-        found = resolve("/articles/new/")
-        self.assertEqual(article_new, found.func)
+    def setUp(self):
+        self.user = UserModel.objects.create(
+            username="test_user",
+            email="test@example.com",
+            password="top_secret_pass0001",
+        )
+        self.client.force_login(self.user)
+
+    def test_render_creation_form(self):
+        response = self.client.get("/articles/new/")
+        self.assertContains(response, "ブログ記事の新規作成", status_code=200)
+    
+    def test_create_article(self):
+        data = {'title': 'タイトル', 'abstract': '概要', 'body': '本文'}
+        self.client.post('/articles/new/', data)
+        article = Article.objects.get(title='タイトル')
+        self.assertEqual('概要', article.abstract)
+        self.assertEqual('本文', article.body)
 
 
 class ArticleDetailTest(TestCase):
-    def test_should_resolve_article_detail(self):
-        found = resolve("/articles/1/")
-        self.assertEqual(article_detail, found.func)
+    def setUp(self):
+        self.user = UserModel.objects.create(
+            username="test_user",
+            email="test@example.com",
+            password="top_secret_pass0001",
+        )
+        self.article = Article.objects.create(
+            title="title1",
+            abstract="abstract",
+            body="body",
+            created_by=self.user
+        )
+
+    def test_should_use_expected_template(self):
+        response = self.client.get("/articles/%s/" % self.article.id)
+        self.assertTemplateUsed(response, "articles/article_detail.html")
+    
+    def test_should_return_200_and_expected_heading(self):
+        response = self.client.get("/articles/%s/" % self.article.id)
+        self.assertContains(response, self.article.title, status_code=200)
 
 
-class EditArticleTest(TestCase):
-    def test_should_resolve_article_edit(self):
-        found = resolve("/articles/1/edit")
-        self.assertEqual(article_edit, found.func)
+# class EditArticleTest(TestCase):
+#     def test_should_resolve_article_edit(self):
+#         found = resolve("/articles/1/edit")
+#         self.assertEqual(article_edit, found.func)
